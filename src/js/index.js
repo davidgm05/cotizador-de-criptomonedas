5c5b6ac65963f829beabf60e37abf2b1940a1460
davidgm05 <davidgutierrezmillan05@gmail.com>
2024-04-08 00:57:20 +0200

    1

--- src/js/index.js
+++ src/js/index.js
@@ -1,94 +1,38 @@
 
 import '../scss/style.scss';
 
-const form = document.querySelector('#form-search');
-const moneda = document.querySelector('#moneda');
-const criptomoneda = document.querySelector('#criptomonedas');
-const containerAnswer = document.querySelector('.container-answer');
-const containerForm = document.querySelector('.form-side');
-const objBusqueda = {
-    moneda: '',
-    criptomoneda: ''
-}
+document.addEventListener("DOMContentLoaded", async function() {
+  const apiKey = '87fef08eba6347379394561770e13cd4';
+  const url = `https://newsapi.org/v2/top-headlines?category=technology&apiKey=${apiKey}`;
 
-document.addEventListener('DOMContentLoaded', () => {
-    consultarCripto();
+  try {
+      const response = await fetch(url);
+      if (!response.ok) {
+          throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      console.log(data);
+      
+      const newsContainer = document.getElementById('news-container'); // Obtener el contenedor donde se mostrarán las noticias
+      
+      
+      data.articles.forEach(article => {
+          // Crear un h2 para el título de la noticia
+          const divElement = document.createElement('div')
+          divElement.className = 'contentBox';
+          const titleElement = document.createElement('h2');
+          titleElement.className = 'title';
+          titleElement.textContent = article.title;
+          
+          // Agregar el h2 al contenedor de noticias
+          divElement.appendChild(titleElement);
+          newsContainer.appendChild(divElement);
+      });
+  } catch (error) {
+      console.error('There was a problem with your fetch operation:', error);
+  }
+});
 
-    form.addEventListener('submit', submitForm);
-    moneda.addEventListener('change', getValue);
-    criptomoneda.addEventListener('change', getValue);
-})
 
-function getValue(e){
-    objBusqueda[e.target.name] = e.target.value;
-}
-
-function submitForm(e){
-    e.preventDefault();
-    const {moneda, criptomoneda} = objBusqueda;
-    if(moneda === ''|| criptomoneda === ''){
-        showError('seleccione ambas casillas');
-        return;
-    }
-    consultApi(moneda, criptomoneda);
-    
-}
-
-function consultApi(moneda, criptomoneda){
-    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
-    fetch(url)
-    .then(respuesta => respuesta.json())
-    .then(respuestaJson => {
-        mostrarValores(respuestaJson.DISPLAY[criptomoneda][moneda]);
-    })
-    .catch(error => console.log(error));
-}
-
-function cleanHtml(){
-    containerAnswer.innerHTML = '';
-}
-
-function mostrarValores(data){
-    cleanHtml();
-    const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE} = data;
-    const answer = document.createElement('div');
-    answer.classList.add('display-info');
-    answer.innerHTML = `<p class="main-price">Precio: <span>${PRICE}</span></p>
-    <p>Precio más alto del día:: <span>${HIGHDAY}</span></p>
-    <p>Precio más bajo del día: <span>${LOWDAY}</span></p>
-    <p>Variación últimas 24 horas: <span>${CHANGEPCT24HOUR}</span></p>
-    <p>Última Actualización: <span>${LASTUPDATE}</span></p>`
-    containerAnswer.appendChild(answer);
-
-}
-
-function showError(mensage){
-    const errorMensage = document.createElement('p');
-    errorMensage.classList.add('error');
-    errorMensage.textContent = mensage;
-    containerForm.appendChild(errorMensage);
-    setTimeout(() => {
-        errorMensage.remove()
-    }, 3000)
-}
-
-function consultarCripto(){
-    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
-    fetch(url)
-    .then(respuesta => respuesta.json())
-    .then(respuestaJson => {
-        selectCripto(respuestaJson.Data);
-    })
-    .catch(error => console.log(error));
-}
-
-function selectCripto(criptos){
-    criptos.forEach(cripto => {
-        const {FullName, Name} = cripto.CoinInfo;
-        const option = document.createElement('option');
-        option.value = Name;
-        option.textContent = FullName;
-        criptomoneda.appendChild(option);
-    });
-}
+  
 
